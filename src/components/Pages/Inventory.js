cdd382d34eaee12d49a050792bd06482a1e9c22f
RaengC <raengc@yahoo.com>
2020-11-29 11:50:00 +1100

    clear form on submit

--- src/components/Pages/Inventory.js
+++ src/components/Pages/Inventory.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Pages.css'
 
-export const Inventory = () => (
-    <div className="page-container">
-        <h1 className="page-title">Pantry Inventory</h1>
-    </div>
-);
+export const Inventory = () => {
+    return (
+        <div className="page-container">
+            <h1 className="page-title">Pantry Inventory</h1>
+        </div>
+    );
+}
--- src/components/Pages/NewItem.js
+++ src/components/Pages/NewItem.js
@@ -4,15 +4,21 @@ import { ItemExpiry } from './ItemExpiry';
 import './Pages.css'
 
 export function NewItem(props) {
-    const [name, setName] = useState("");
-    const [category, setCategory] = useState("");
-    const [location, setLocation] = useState("");
-    const [amount, setAmount] = useState("");
-    const [expiry, setExpiry] = useState("");
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState('');
+    const [location, setLocation] = useState('');
+    const [amount, setAmount] = useState('');
+    const [expiry, setExpiry] = useState('');
 
-    const handleSubmit = (e) => {
+const handleSubmit = (e) => {
         e.preventDefault();
-        alert(`Submitting Name ${name}`);
+        console.log(name, category, location, amount, expiry);
+        
+        setName('')
+        setCategory('')
+        setLocation('')
+        setAmount('')
+        setExpiry('')
     } 
 
     return (
@@ -21,15 +27,9 @@ export function NewItem(props) {
                 <h1 className="page-title">New Item </h1>
             </div>
 
-            <div className="form-container">
-                <form action="action_page.php">
-                    <div className="form-row">
-                        <lable 
-                            onSubmit={handleSubmit}
-                            className="form-label"
-                            >
-                            Item Name: 
-                        </lable>         
+            <form onSubmit={handleSubmit} className="form-container">
+                <div className="form-row">
+                    <lable className="form-label"> Item Name: </lable>         
                         <input 
                             type="text"
                             placeholder="Enter item name"
@@ -38,82 +38,62 @@ export function NewItem(props) {
                             onChange={e => setName(e.target.value)} 
                         />
                   
-                    </div>
+                </div>
 
-                    <div className="form-row">
-                        <label
-                            for="category"
-                            className="form-label"
-                            >
-                            Category
-                        </label>
-                    </div>
-                    <select 
-                        id="food-category" 
-                        name="food-category"
+                <div className="form-row">
+                    <label className="form-label"> Category </label>
+                </div>
+                <select 
+                    id="food-category" 
+                    name="food-category"
+                    required
+                    value={category}
+                    onChange={e => setCategory(e.target.value)}>
+                        <option 
+                            value={category}
+                            > Select your category
+                        </option>
+                        <option value="fresh">Fresh/Room Temperature</option>
+                        <option value="refrigerated"> Refrigerated</option>
+                        <option value="frozen">Frozen</option>
+                        <option value="pantry-items"> Pantry Items/Dry Goods</option>
+                        
+                </select>
+
+                <div className="form-row">
+                    <lable className="form-label"> Location: </lable>         
+                    <input 
+                        type="text"
+                        placeholder="Enter location e.g. kitchen fridge, top shelf"
                         required
-                        onChange={e => setCategory(e.target.value)}>
-                            <option 
-                                value={category}
-                                onSubmit={handleSubmit}
-                                > Select your category
-                            </option>
-                            <option value="fresh">Fresh/Room Temperature</option>
-                            <option value="refrigerated"> Refrigerated</option>
-                            <option value="frozen">Frozen</option>
-                            <option value="pantry-items"> Pantry Items/Dry Goods</option>
-                            
-                    </select>
-
-                    <div className="form-row">
-                        <lable 
-                            onSubmit={handleSubmit}
-                            className="form-label">
-                                Location:  
-                        </lable>         
-                        <input 
-                            type="text"
-                            placeholder="Enter location e.g. kitchen fridge, top shelf"
-                            required
-                            value={location}
-                            onChange={e => setLocation(e.target.value)} 
-                        />
-                    </div>               
+                        value={location}
+                        onChange={e => setLocation(e.target.value)} 
+                    />
+                </div>               
                     
-                    <div className="form-row">
-                        <lable 
-                            onSubmit={handleSubmit}
-                            className="form-label"
-                            >
-                            Amount/Volume:   
-                        </lable>         
-                        <input 
-                            type="text"
-                            placeholder="Enter the size/volume of your item e.g. 1 bunch, 400gm, single chicken breast."
-                            required
-                            value={amount}
-                            onChange={e => setAmount(e.target.value)} 
-                        />
-                    </div>
+                <div className="form-row">
+                    <lable className="form-label"> Amount/Volume: </lable>         
+                    <input 
+                        type="text"
+                        placeholder="Enter the size/volume of your item e.g. 1 bunch, 400gm, single chicken breast."
+                        required
+                        value={amount}
+                        onChange={e => setAmount(e.target.value)} 
+                    />
+                </div>
                 
-                {/* TODO: add placeholder for date, tried in ItemExpiry but not workign there either, docs say it should! */}
-                    <div className="form-row">
-                        <lable 
-                            onSubmit={handleSubmit} 
-                            className="form-label" >
-                        Select Expiry Date:    
-                        </lable>    
-                        <ItemExpiry 
-                            value={expiry}
-                            onChange={date => setExpiry(date)} />     
-                    </div>
+            {/* TODO: add placeholder for date, tried in ItemExpiry but not workign there either, docs say it should! */}
+                <div className="form-row">
+                    <lable className="form-label">Select Expiry Date: </lable>    
+                    <ItemExpiry 
+                        value={expiry}
+                        onChange={date => setExpiry(date)} />     
+                </div>
 
-                <input 
-                    type="submit" 
+                <input type="submit" 
                     className="submit-btn"
                     value="Submit"/>
-                </form>
-            </div>
+            </form>
         </Fragment>
     );
 }
