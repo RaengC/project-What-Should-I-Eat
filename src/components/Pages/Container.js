0720419d278014065e65ca295968574459d1a5c5
RaengC <raengc@yahoo.com>
2020-11-30 17:27:08 +1100

    localStorage working, saving items as objects within an array

--- src/components/Pages/Container.js
+++ src/components/Pages/Container.js
@@ -1,36 +1,18 @@
-import React, { Component, Fragment } from 'react';
-import { NewItemForm } from './NewItem/NewItemForm';
+import React, { Fragment } from 'react';
 
-class Container extends Component {
-    constructor() {
-        super();
-        this.state = {
-            items: []
-        };
-        this.addItem = this.addItem.bind(this);
-    }
+// pass data from local storage from Container to Home and Inventory
 
-    addItem(content) {
-        axios.post(localStorage, {content: content}).then((result) => {
-            console.log(result.data);
-            this.setState({items: [...this.state.items]})
-        })
-    }
-
-    render() {
-        return (
-            <Fragment>
-                {/* <Home /> */}
-                <NewItemForm />
-                    
-                    
-            </Fragment>
-        )
-    }
+export const Container = () => {
+    
+    return (
+        <Fragment>
+            <Home />
+            <Inventory />
+                
+        </Fragment>
+    )
 }
 
-export default Container;
 
-
-// an array of objects, each item. 
+ 
 
--- src/components/Pages/NewItem/NewItem.js
+++ src/components/Pages/NewItem/NewItem.js
@@ -3,34 +3,44 @@ import { NewItemForm } from './NewItemForm';
 
 import '../Pages.css'
 
+ // Local Storage. TODO make DRY (this function also in NewItemForm)
+ function useLocalStorage(key, initialValue) {
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            console.log(error);
+            return initialValue;
+        }
+    });
+
+    const setValue = value => {
+        try {
+            const valueToStore =
+            value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return [storedValue, setValue];
+}
+
 export const NewItem = () => {
-    const [item, setItem] = useState([])
+    const [items, setItems] = useLocalStorage('items', '')
 
-    console.log('newitem passing', item)
-     
-    // addItem( () => {
-    //     // localStorage.getItem(content)
-    //     // console.log(content)
-    // })
-    // {
-        // localStorage.getItem(content)
-        // console.log(content)
-        // axios.post(localStorage, {content: content}).then((result) => {
-        //     console.log(result.data);
-        //     setItems(result.data)
-        // })
-    // }
-
+    const addItem =(item) => {
+        setItems([...items, item])
+    }
         return (
             <Fragment>
                 <div className="page-container">
                     <h1 className="page-title">New Item </h1>
                 </div>
                 
-          
-    
-                <NewItemForm onSubmit={ setItem }
-                />
+                <NewItemForm onSubmit={ addItem } />
             </Fragment>
         )
 
