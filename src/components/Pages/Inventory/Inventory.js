813d2e4b35ee6848d9ad4aad0a75b5e7d30fc53a
RaengC <raengc@yahoo.com>
2020-12-01 22:20:36 +1100

    sorting form created, but need to add item key for each object for it to function

--- src/components/Pages/Inventory/Inventory.js
+++ src/components/Pages/Inventory/Inventory.js
@@ -1,11 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useMemo } from 'react';
 import Table from './Table'
 
 import '../Pages.css'
 
 export const Inventory = (props) => {
-
-    console.log(props)
+    const { items } = props;
+    const [sortConfig, setSortConfig] = useState(null);
+    
+    useMemo( () => {
+        let sortedItems = [...items];
+       
+        if (sortConfig !== null) {
+            sortedItems.sort((a, b) => {
+                if (a[sortConfig.key] < b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? -1 : 1;
+                }
+                if (a[sortConfig.key] > b[sortConfig.key]) {
+                    return sortConfig.direction === 'ascending' ? 1 : -1;
+                }
+                return 0;
+            });
+        }
+        return sortedItems;
+    }, [items, sortConfig]);
+    
+    const requestSort = (key) => {
+        let direction = 'ascending';
+        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending';
+        }
+        setSortConfig({ key, direction });
+    };
 
     return (
         <Fragment>
@@ -18,18 +43,38 @@ export const Inventory = (props) => {
             <table className="table">
                 <thead className="table-header">
                     <tr>
-                        <th>Name </th>
-                        <th>Category </th>
-                        <th>Location</th>
-                        <th>Amount </th>
-                        <th>Expiry</th>
+                        <th>
+                            <button type="button" onClick={ () => requestSort('name')}>
+                            Name 
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" onClick={ () => requestSort('category')}>
+                            Category 
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" onClick={ () => requestSort('location')}>
+                            Location 
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" onClick={ () => requestSort('amount')}>
+                            Amount 
+                            </button>
+                        </th>
+                        <th>
+                            <button type="button" onClick={ () => requestSort('expiry')}>
+                            Expiry 
+                            </button>
+                        </th>
                     </tr>
                 </thead>
                 <tbody>
-                    {props.items.map((item) => {
-                     
+                    {items.map(item => {
                         return (
-                            <tr key={item._id}>
+                            // my objects dont have an item ID ... the orginal code may have to be changed so I can then sort! 
+                            <tr key={item.id}>
                                 <th>{item.name}</th>
                                 <th>{item.category}</th>
                                 <th>{item.location}</th>
@@ -41,7 +86,6 @@ export const Inventory = (props) => {
                 </tbody>
             </table>
             </div>
-  
 
         </Fragment>
     );
